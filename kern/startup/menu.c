772632bdaf28c58d0831c262a25545ea2f30048a
mengtianao <t5meng@uwaterloo.ca>
2020-12-26 13:08:03 -0500

    A0-q2

--- kern/startup/menu.c
+++ kern/startup/menu.c
@@ -273,6 +273,17 @@ cmd_panic(int nargs, char **args)
 	return 0;
 }
 
+static
+int
+cmd_dth()
+{
+	(void)nargs;
+	(void)args;
+	
+	dbflags = DB_THREADS
+	return 0;
+}
+
 /*
  * Command for shutting down.
  */
@@ -436,7 +447,7 @@ static const char *opsmenu[] = {
 	"[pwd]     Print current directory   ",
 	"[sync]    Sync filesystems          ",
 	"[panic]   Intentional panic         ",
-    "[dth]   Enable DB_THREADS debuggin messages",
+    "[dth]   Enable DB_THREADS debuging messages",
 	"[q]       Quit and shut down        ",
 	NULL
 };
@@ -547,6 +558,7 @@ static struct {
 	{ "pwd",	cmd_pwd },
 	{ "sync",	cmd_sync },
 	{ "panic",	cmd_panic },
+	{ "dth", cmd_dth },
 	{ "q",		cmd_quit },
 	{ "exit",	cmd_quit },
 	{ "halt",	cmd_quit },
