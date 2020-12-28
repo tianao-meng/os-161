bb6f849c1fc3a88cbf5b2366ec9d8e6f5a7dc9f9
mengtianao <t5meng@uwaterloo.ca>
2020-12-28 11:20:46 -0500

    A1

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -295,13 +295,13 @@ void
 cv_wait(struct cv *cv, struct lock *lock)
 {
         // Write this
-    spinlock_acquire(&cv -> cv_lock);
+    //spinlock_acquire(&cv -> cv_lock);
     lock_release(lock);
-    spinlock_release(&cv -> cv_lock);
+    //spinlock_release(&cv -> cv_lock);
     wchan_sleep(cv->cv_wchan);
-    spinlock_acquire(&cv -> cv_lock);
+    //spinlock_acquire(&cv -> cv_lock);
     lock_acquire(lock);
-    spinlock_release(&cv -> cv_lock);
+    //spinlock_release(&cv -> cv_lock);
 }
 
 void
