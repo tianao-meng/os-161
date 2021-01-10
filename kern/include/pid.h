f9931892406c86720a4e56d57af9a22a99be4a94
mengtianao <t5meng@uwaterloo.ca>
2021-01-10 16:54:26 -0500

    A2a

--- kern/include/pid.h
+++ kern/include/pid.h
@@ -24,8 +24,8 @@ bool check_if_in_pid_array(struct proc_id * proc);
 int find_available_pos(void);
 struct proc_id * get_proc_pid(pid_t pid);
 struct proc_id * get_proc_pid_inbuffer(pid_t pid);
-void allocate_pid(struct proc_id * parent, struct proc_id ** childret);
-int wait(struct proc_id * parent, pid_t child_pid, int * childret);
+void allocate_pid(struct proc_id * parent, struct proc_id * child_return);
+int wait(struct proc_id * parent, pid_t child_pid, struct proc_id ** childret);
 void exit(struct proc_id * proc, int exitcode);
 void delete_pid(struct proc_id * proc);
 
