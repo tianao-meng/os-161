2d7985c2bea063195afd0739d63a9e53fe42860f
mengtianao <t5meng@uwaterloo.ca>
2021-01-11 16:39:34 -0500

    A2a

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -97,7 +97,7 @@ struct proc *p = curproc;
 #if OPT_A2
 exit(curproc -> pid, exitcode);
 curproc -> pid = NULL;
-DEBUG(MY,"i am in myself debug mode");
+DEBUG(MY,"i am in myself debug mode\n");
 
 #endif
 
