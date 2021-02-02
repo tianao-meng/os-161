6a71a6059d59fd014430a6d712fd34fc9437c06d
mengtianao <t5meng@uwaterloo.ca>
2021-02-02 15:34:20 -0500

    A2b

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -248,9 +248,6 @@ int sys_execv(const char *progname_uspace, char ** args_uspace){
 
   /* Create a new address space. */
   as_new = as_create();
-  kprintf("0: %s \n", args_kspace[0]);
-  kprintf("1: %s \n", args_kspace[1]);
-  kprintf("2: %s \n", args_kspace[2]);
   if (as_new == NULL) {
     vfs_close(v);
     // for (size_t j = 0; j < (execv_args_len + 1); j++){
@@ -301,6 +298,10 @@ int sys_execv(const char *progname_uspace, char ** args_uspace){
     //kfree(progname_kspcae);
     return result;
   }
+
+  kprintf("0: %s \n", args_kspace[0]);
+  kprintf("1: %s \n", args_kspace[1]);
+  kprintf("2: %s \n", args_kspace[2]);
 
   vaddr_t args_userspace[execv_args_len + 1];
 
