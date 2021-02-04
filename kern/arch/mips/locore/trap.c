e2fc780716f52024c67da4e41edbcde65dc1bfb0
mengtianao <t5meng@uwaterloo.ca>
2021-02-03 21:27:20 -0500

    A3

--- kern/arch/mips/locore/trap.c
+++ kern/arch/mips/locore/trap.c
@@ -113,6 +113,9 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 
 #if OPT_A3
 	sys__exit(sig);
+	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
+		code, sig, trapcodenames[code], epc, vaddr);
+	panic("I don't know how to handle this\n");
 #else
 
 	/*
