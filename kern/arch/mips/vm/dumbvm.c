cbf646c35712f4437b544b1ac18d4ec97a1f3fbc
mengtianao <t5meng@uwaterloo.ca>
2021-02-03 20:14:42 -0500

    A3

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -121,7 +121,7 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 	switch (faulttype) {
 	    case VM_FAULT_READONLY:
 	#if OPT_A3
-	    return EPERM
+	    return EFAULT;
 	#endif
 		/* We always create pages read-write, so we can't get this */
 		panic("dumbvm: got VM_FAULT_READONLY\n");
