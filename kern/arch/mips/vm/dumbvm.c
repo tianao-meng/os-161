77ef30de81a42d035f51b3afdc4b1d6062089263
mengtianao <t5meng@uwaterloo.ca>
2021-02-03 20:43:09 -0500

    A3

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -127,7 +127,7 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 	    /* We always create pages read-write, so we can't get this */
 		panic("dumbvm: got VM_FAULT_READONLY\n");
 
-	#endif
+	//#endif
 
 
 	    case VM_FAULT_READ:
