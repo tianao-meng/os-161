18449338d9ba092d0f1667aed8fb827642d6a030
mengtianao <t5meng@uwaterloo.ca>
2021-02-05 21:38:03 -0500

    A3

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -156,7 +156,7 @@ paddr_t vm_stealmem(unsigned long npages) {
 				for (unsigned long npages_to_allocate = 0; npages_to_allocate < npage_can_allocate; npages_to_allocate++){
 
 					((int *) PADDR_TO_KVADDR(coremap_start))[cur_page + npages_to_allocate] = ((int)npages_to_allocate + 1);
-					kprintf("%s%d\n","cur_page + npages_to_allocate : " , (int *) PADDR_TO_KVADDR(coremap_start))[cur_page + npages_to_allocate]);
+					kprintf("%s%d\n","cur_page + npages_to_allocate : " , ((int *) PADDR_TO_KVADDR(coremap_start))[cur_page + npages_to_allocate]);
 					//*(int *)(coremap_start + (npages_to_allocate * sizeof(int))) = npages_to_allocate + 1;
 
 				}
